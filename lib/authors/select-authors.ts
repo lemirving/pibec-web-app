8b41e1f13d56c0cb2928be735a85d78e412a1bab
Irving <dev.lemirving@gmail.com>
2026-06-09 12:13:26 -0400

    feat: implementa funções SELECT para db

---
+++ lib/authors/select-authors.ts
@@ -0,0 +1,19 @@
+import { db } from "@/db"
+import {authors, classrooms} from "@/db/schema"
+import { eq , desc} from "drizzle-orm";
+
+
+export async function getAllAuthors() {
+  return await db
+    .select({
+      id: authors.id,
+      name: authors.name,
+      classroom: classrooms.name,
+      educationLevel: authors.educationLevel,
+      grade: authors.grade,
+      createdAt: authors.createdAt,
+    })
+    .from(authors)
+    .leftJoin(classrooms, eq(authors.classroomId, classrooms.id))
+    
+}
